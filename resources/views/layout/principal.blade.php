af60a9966cf54d2574f730466e0ea76611c30a93
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-01-06 23:19:15 -0300

    adicionando navegacao e rodape

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -4,12 +4,34 @@
     <meta charset="UTF-8">
     <title>Controle de estoque</title>
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/custom.css">
 </head>
 <body>
 <div class="container">
 
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/produtos">
+                    Estoque Laravel
+                </a>
+            </div>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="/produtos">Listagem</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+
     @yield('conteudo')
 
+    <footer class="footer">
+        <p>(c) Livro de Laravel Casa do Código</p>
+    </footer>
 </div>
 </body>
 </html>
