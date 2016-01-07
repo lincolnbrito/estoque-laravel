2679f8c990cda4726337c3a8db6c0a4cdcae6ea0
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-01-06 21:46:25 -0300

    criando model Produto

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -1,9 +1,23 @@
 <?php namespace estoque\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class ProdutosController extends Controller
 {
     public function lista()
     {
-        return '<h1>Listagem de produtos com Laravel</h1>';
+        $html = '<h1>Listagem de produtos com Laravel</h1>';
+
+        $html .= '<ul>';
+
+        $produtos = DB::select('select * from produtos');
+
+        foreach ($produtos as $produto) {
+            $html .= '<li> Nome '. $produto->nome .', Descrição: '. $produto->descricao .'</li>';
+        }
+
+        $html .= '</ul>';
+
+        return $html;
     }
 }
